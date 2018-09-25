ee4592e907b55bcbda606d7d6793e83393ab5c80
johnnie.short <johnnie.short@gmail.com>
2018-09-25 21:52:18 +0300

    logging

--- service/src/main/java/com/itacademy/jd2/ikarotki/rwmanager/service/TicketServiceImpl.java
+++ service/src/main/java/com/itacademy/jd2/ikarotki/rwmanager/service/TicketServiceImpl.java
@@ -1,55 +1,58 @@
 package com.itacademy.jd2.ikarotki.rwmanager.service;
+
 import java.util.Date;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.itacademy.jd2.ikarotki.rwmanager.dao.api.ITicketDao;
 import com.itacademy.jd2.ikarotki.rwmanager.dao.api.entity.ITicket;
 import com.itacademy.jd2.ikarotki.rwmanager.dao.jdbc.impl.TicketDaoImpl;
 
-
-
-
 public class TicketServiceImpl implements ITicketService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TicketServiceImpl.class);
+	private ITicketDao dao = new TicketDaoImpl();
 
-    private ITicketDao dao=new TicketDaoImpl();
+	@Override
+	public ITicket createEntity() {
+		return dao.createEntity();
+	}
 
-    @Override
-    public ITicket createEntity() {
-        return dao.createEntity();
-    }
+	@Override
+	public void save(final ITicket entity) {
+		final Date modifedOn = new Date();
+		entity.setUpdated(modifedOn);
+		if (entity.getId() == null) {
+			LOGGER.info("new brand created:" + entity);
+			entity.setCreated(modifedOn);
+			dao.insert(entity);
+		} else {
+			LOGGER.info("brand updated:" + entity);
+			dao.update(entity);
+		}
+	}
 
-    @Override
-    public void save(final ITicket entity) {
-        final Date modifedOn = new Date();
-        entity.setUpdated(modifedOn);
-        if (entity.getId() == null) {
-            entity.setCreated(modifedOn);
-            dao.insert(entity);
-        } else {
-            dao.update(entity);
-        }
-    }
+	@Override
+	public ITicket get(final Integer id) {
+		final ITicket entity = dao.get(id);
+		return entity;
+	}
 
-    @Override
-    public ITicket get(final Integer id) {
-        final ITicket entity = dao.get(id);
-        return entity;
-    }
+	@Override
+	public void delete(final Integer id) {
+		dao.delete(id);
+	}
 
-    @Override
-    public void delete(final Integer id) {
-        dao.delete(id);
-    }
+	@Override
+	public void deleteAll() {
+		dao.deleteAll();
+	}
 
-    @Override
-    public void deleteAll() {
-        dao.deleteAll();
-    }
-
-    @Override
-    public List<ITicket> getAll() {
-        final List<ITicket> all = dao.selectAll();
-        return all;
-    }
+	@Override
+	public List<ITicket> getAll() {
+		final List<ITicket> all = dao.selectAll();
+		return all;
+	}
 
 }
--- service/src/test/java/com/itacademy/jd2/ikarotki/rwmanager/service/AbstractTest.java
+++ service/src/test/java/com/itacademy/jd2/ikarotki/rwmanager/service/AbstractTest.java
@@ -2,6 +2,8 @@ package com.itacademy.jd2.ikarotki.rwmanager.service;
 
 import java.util.Random;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import com.itacademy.jd2.ikarotki.rwmanager.dao.api.entity.ITicket;
 import com.itacademy.jd2.ikarotki.rwmanager.service.ITicketService;
 import com.itacademy.jd2.ikarotki.rwmanager.service.TicketServiceImpl;
@@ -11,7 +13,12 @@ public abstract class AbstractTest {
     protected ITicketService ticketService = new TicketServiceImpl();
 
     private static final Random RANDOM = new Random();
-
+    @BeforeEach
+    public void setUpMethod() {
+        // clean DB recursive
+        //TicketService.deleteAll();
+        //TicketService.deleteAll();
+    }
     protected String getRandomPrefix() {
         return RANDOM.nextInt(99999) + "";
     }
