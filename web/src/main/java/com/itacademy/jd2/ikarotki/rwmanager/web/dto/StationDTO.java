1d54de93199d249599bf1ce592ea595faaa7b741
DESKTOP-RRRJ1VN\dz <dmitri.zhyvushko@gmail.com>
2018-11-11 00:35:39 +0300

    no message

--- web/src/main/java/com/itacademy/jd2/ikarotki/rwmanager/web/dto/StationDTO.java
+++ web/src/main/java/com/itacademy/jd2/ikarotki/rwmanager/web/dto/StationDTO.java
@@ -6,78 +6,72 @@ import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.hibernate.validator.constraints.Range;
-
 public class StationDTO {
-	@NotNull
-	@Range(min=0)
-	private Integer id;
-	@NotNull
-	@Size(min = 1, max = 50)
-	private String name;
-	@NotNull
-	@Digits(integer=8, fraction=6)
-	private Double longitude;
-	@NotNull
-	@Digits(integer=8, fraction=6)
-	private Double latitude;
-	@NotNull
-	private Date created;
-	@NotNull
-	private Date updated;
+    private Integer id;
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String name;
+    @NotNull
+    @Digits(integer = 8, fraction = 6)
+    private Double longitude;
+    @NotNull
+    @Digits(integer = 8, fraction = 6)
+    private Double latitude;
+    private Date created;
+    private Date updated;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public Date getCreated() {
-		return created;
-	}
+    public Date getCreated() {
+        return created;
+    }
 
-	public void setCreated(Date created) {
-		this.created = created;
-	}
+    public void setCreated(Date created) {
+        this.created = created;
+    }
 
-	public Date getUpdated() {
-		return updated;
-	}
+    public Date getUpdated() {
+        return updated;
+    }
 
-	public void setUpdated(Date updated) {
-		this.updated = updated;
-	}
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
 
-	public Double getLatitude() {
-		return latitude;
-	}
+    public Double getLatitude() {
+        return latitude;
+    }
 
-	public void setLatitude(Double latitude) {
-		this.latitude = latitude;
-	}
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Double getLongitude() {
-		return longitude;
-	}
+    public Double getLongitude() {
+        return longitude;
+    }
 
-	public void setLongitude(Double coordinates) {
-		this.longitude = coordinates;
-	}
+    public void setLongitude(Double coordinates) {
+        this.longitude = coordinates;
+    }
 
-	@Override
-	public String toString() {
-		return "StationDTO [id=" + id + ", name=" + name + ", longitude=" + longitude + ", latitude=" + latitude
-				+ ", created=" + created + ", updated=" + updated + "]";
-	}
+    @Override
+    public String toString() {
+        return "StationDTO [id=" + id + ", name=" + name + ", longitude=" + longitude + ", latitude=" + latitude
+                + ", created=" + created + ", updated=" + updated + "]";
+    }
 
 }
